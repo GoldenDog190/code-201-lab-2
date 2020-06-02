afcef9a028d17d5188fe8b4cd409af6dbd1f6210
Amelia Waggoner <wildginger@wavecable.com>
2020-06-02 13:56:12 -0700

    code 201 lab 2 first commit of html, css, & js files

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/*
+About Amelia 
+
+Has Amelia studied Japanese?
+if they are right
+  yes, she has
+else
+ prompt try again
+
+Does Amelia live in WA?
+if they are right 
+  Yes, she lives in WA
+else 
+prompt wrong
+
+Has Amelia lived in Japan?
+if they are right
+  yes, she has lived in Japan
+else
+prompt She hasn't just lived in the U.S.
+
+Does Amelia love animals?
+if they are right
+ of course she does
+else
+prompt who doesn't love animals
+
+Has Amelia volunteered before?
+if they are right 
+  yes she has
+else
+prompt everyone should volunteer at least once
+
+Change all the prompts to alerts
+*/
+
