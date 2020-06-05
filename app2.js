10b859d1e2e637f3fb582e58c8ec7e42bf197d1f
Amelia Waggoner <wildginger@wavecable.com>
2020-06-04 20:09:57 -0700

    made a new question 7 because the other one wasn't quite what I wanted it to be

--- app2.js
+++ app2.js
@@ -107,31 +107,50 @@ function twentyEight() {
 twentyEight();
 
 // // //=========Question 7=============
-
 function placesTraveled() {
+  var japanArray = ['tokyo', 'kyoto', 'nara', 'osaka', 'mt. fuji','kamakura'];
 
-  var counter = 0;
-  place: while(counter < 6){
-    var japanPlace, jLen;
-    japanPlace = ['Tokyo','Kyoto','Nara','Osaka','Sapporo','Kamakura'];
-    jLen = japanPlace.length;
-    for(var i = 0; i < jLen; i++) {
-      var answerGuess = prompt('Guess which places I have been to?');
-      //alert(japanPlace[i]);
-      if(answerGuess === japanPlace[i]){
-        alert(japanPlace[i] + 'is correct.')
-        break place;
-      } else {
-        alert ('Guess Again');
-      }
+  for (var i = 0; i < 7; i++){
+    var japans = propmpt('What cities do you think Amelia has been to?').toLowerCase(); 
+    if(japans === japanArray[0] || japans === japanArray[1] || japans === japanArray[2] || japans === japanArray[3] || japans === japanArray[4] || japans === japanArray[5] || japans === japanArray[6]){
+      console.log('yes');
+      score++;
+      alert('correct');
+    } else {
+      console.log('no');
+      alert('wrong');
     }
   }
-  
-  alert('You answered correctly' + 'These are all the places I have been to' + japanPlace + ' in Japan.');
-
 }
-
 placesTraveled();
+alert('You answered' + score + 'correcty.');
+alert('Good job!');
+
+// Other question 7 
+// function placesTraveled() {
+
+//   var counter = 0;
+//   place: while(counter < 6){
+//     var japanPlace, jLen;
+//     japanPlace = ['Tokyo','Kyoto','Nara','Osaka','Sapporo','Kamakura'];
+//     jLen = japanPlace.length;
+//     for(var i = 0; i < jLen; i++) {
+//       var answerGuess = prompt('Guess which places I have been to?');
+//       //alert(japanPlace[i]);
+//       if(answerGuess === japanPlace[i]){
+//         alert(japanPlace[i] + 'is correct.')
+//         break place;
+//       } else {
+//         alert ('Guess Again');
+//       }
+//     }
+//   }
+  
+//   alert('You answered correctly' + 'These are all the places I have been to' + japanPlace + ' in Japan.');
+
+// }
+
+//placesTraveled();
 
 //====Score===
 
@@ -157,5 +176,3 @@ alert('You answered' + score + 'correctly');
 var userName = prompt('What\'s your name?');
 //   /*console.log('Have a nice day!' + userName);*/
  alert('Have a nice day! ' + userName);
-
-
